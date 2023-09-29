d40bc2d4b0d190871be502fc4e1e67c5fe90b958
Tobias Hofmaenner <tobias.hofmaenner@ferrovac.com>
2023-09-29 11:43:39 +0200

    Created LscOS lib

---
+++ LscOS/LscOS.h
@@ -0,0 +1,18 @@
+/*
+AUTHOR:       Tobias Hofmänner
+DATE:         29.09.2023
+USAGE:        TODO
+DESCRIPTION:  TODO
+TODO:       
+RECOURCES:  TODO  
+*/
+
+#ifndef LscOS_H
+#define LscOS_H
+
+#include <Arduino.h>
+
+#include "LscError.h"
+
+
+#endif
