0e387f3f46b7fda677d1c7c23ecfb38b9c2e5820
Tobias Hofmaenner <tobias.hofmaenner@ferrovac.com>
2023-09-29 11:46:59 +0200

    created LscComponents lib

---
+++ LscComponents/LscComponents.h
@@ -0,0 +1,18 @@
+/*
+AUTHOR:       Tobias Hofmänner
+DATE:         29.09.2023
+USAGE:        TODO
+DESCRIPTION:  TODO
+TODO:       
+RECOURCES:  TODO  
+*/
+
+#ifndef LscComponents_H
+#define LscComponents_H
+
+#include <Arduino.h>
+
+#include "LscError.h"
+
+
+#endif
---
+++ LscOS/LscOS.cpp
@@ -0,0 +1,1 @@
+#include "LscOS.h"
