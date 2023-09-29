f1db93912e1956655ee04ac3bf85cf978c90878d
Tobias Hofmaenner <tobias.hofmaenner@ferrovac.com>
2023-09-29 12:45:58 +0200

    implemented first phyical unit system with enums

--- LscComponents/LscComponents.h
+++ LscComponents/LscComponents.h
@@ -11,8 +11,54 @@ RECOURCES:  TODO
 #define LscComponents_H
 
 #include <Arduino.h>
-
 #include "LscError.h"
+#include <vector>
+
+//A list of all available physical units
+struct Units{
+    //List of all available temperature units
+    struct Temperature {
+        enum struct Unit{
+            //Kelvin
+            K, 
+            //Celsius
+            C, 
+            //Fahrenheit
+            F 
+        };
+        static std::vector<String> getOptions(){
+            return {"Kelvin", "Celsius", "Fahrenheit"};
+        }
+
+    };
+    struct Pressure{
+        //List of all available pressure units
+        enum struct Unit{
+            //mbar
+            mBar,
+            //Pascal
+            Pa,
+            //Torr mmHg
+            Torr,
+            //Pounds per square inch
+            psi,
+            //Atmospheres
+            atm
+        };
+        static std::vector<String> getOptions(){
+            return {"mbar", "Torr mmHg", "Pounds Per Square Inch", "Atmospheres"};
+        }
+    };
+};
+
+
+class TemperatureSensor{
+    private:
+    public:
+        
+        
+
+};
 
 
 #endif
