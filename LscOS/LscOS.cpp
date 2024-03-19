771c78859c47f5bc78b70e2e81c04bed666a5868
Tobias Hofmaenner <tobias.hofmaenner@ferrovac.com>
2024-03-19 11:27:05 +0100

    Added license header

--- LscOS/LscOS.cpp
+++ LscOS/LscOS.cpp
@@ -1,3 +1,23 @@
+/*  Main arduino programm that controlls the Ferrovac GloveBox 
+    Copyright (C) 2024 Ferrovac AG
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    NOTE: This specific version of the license has been chosen to ensure compatibility 
+          with the SD library, which is an integral part of this application and is 
+          licensed under the same version of the GNU General Public License.
+*/
+
 #include "LscOS.h"
 #include "LscPersistence.h"
 
